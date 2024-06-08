297cb1ce0b72d44e054ffa1600749fdb82723f6b
smiurgh9 <simurgh9@pm.me>
2024-06-07 19:30:36 -0500

    changed the move script.

--- src/latex/move.sh
+++ src/latex/move.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-sudo cp -i -v homework.cls hwlst.sty hwsymb.sty hwcmd.sty /usr/local/texlive/texmf-local/tex/latex/local/
+sudo cp -v homework.cls hwlst.sty hwsymb.sty hwcmd.sty /usr/local/texlive/texmf-local/tex/latex/local/
 sudo chown -R $USER /usr/local/texlive/texmf-local/tex/latex/local/
 mktexlsr
